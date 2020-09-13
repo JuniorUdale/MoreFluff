1f63daac9747d1ee029f0f51f74ae43878e6671d
JuniorUdale <junior_udale@hotmail.com>
2020-09-13 10:44:25 +0100

    1.9 before gens

---
+++ _append/data/text/scripts/diceydungeons/jesterpacks/dungeonchanges.hx
@@ -0,0 +1,56 @@
+if (levelupselections.indexOf("boosterpack:Firecracker,Bang Snaps,Bang Snaps") > -1){ 
+  Rules.substitute("Backfire", "Firecracker"); 
+  Rules.substitute("Bop,Bop,Bop", rand(["Sparkler,Firecracker,Sparkler", "Bang Snaps,Bang Snaps,Bang Snaps", "Sparkler,Firecracker,Sparkler"])); 
+}
+if (levelupselections.indexOf("Boosterpack:Mace,Boopin' Stick, Boopin' Stick") > -1){ 
+  Rules.substitute("Backfire", "Chain Whip"); 
+  Rules.substitute("Bop,Bop,Bop", rand(["Liquorice,Staff,Liquorice", "Dice Storage,Crossbow,Dice Storage", "Buckler,Paintbrush,Buckler"])); 
+}
+if (levelupselections.indexOf("Boosterpack:Pristine Sword,Sweets,Sweets") > -1){ 
+  Rules.substitute("Backfire", "Venus Fly Trap"); 
+  Rules.substitute("Bop,Bop,Bop", rand(["Life Rip,Blood Suck,Life Rip", "Life Drain,Piercing Scroll,Life Drain", "Sour Candy,Healing Crystal,Sour Candy"])); 
+}
+if (levelupselections.indexOf("boosterpack:Essence Detonator,Basic Essence,Basic Essence") > -1){ 
+  Rules.substitute("Backfire", "Chromatic Essence"); 
+  Rules.substitute("Bop,Bop,Bop", rand(["Fire Essence,Angelfire,Air Essence", "Ice Essence,Angelfrost,Earth Essence", "Dark Essence,Dark Essence,Dark Essence"])); 
+}
+if (levelupselections.indexOf("Boosterpack:Refund,Reject,Reject") > -1){ 
+  Rules.substitute("Backfire", "Refresh"); 
+  Rules.substitute("Bop,Bop,Bop", rand(["Snake Eye Charm,Reboot,Snake Eye Charm", "Keyblade,Ekans Charm,Keyblade", "Bump,Six Shooter,Bump"])); 
+}
+if (levelupselections.indexOf("boosterpack:Secretive Swipe,Stealth,Stealth") > -1){ 
+  Rules.substitute("Backfire", "Suffocating Smog"); 
+  Rules.substitute("Bop,Bop,Bop", rand(["Stealth,Shadow Stick,Stealth", "Stealth,Smokestaff,Stealth", "Stealth,Smokey Star,Stealth"])); 
+}
+if (levelupselections.indexOf("Boosterpack:Life Siphon,Regen,Regen") > -1){ 
+  Rules.substitute("Backfire", "Leech Seed"); 
+  Rules.substitute("Bop,Bop,Bop", rand(["Sweets,Harmony Orb,Sweets", "Regen,Harmony Orb,Regen", "Harmony Orb,Sweets,Harmony Orb"])); 
+}
+if (levelupselections.indexOf("Boosterpack:Cactus Spear,Rose Bush,Rose Bush") > -1){ 
+  Rules.substitute("Backfire", "Bramble"); 
+  Rules.substitute("Bop,Bop,Bop", rand(["Sour Candy,Spiked Drink,Sour Candy", "Rose Bush,Crown of Thorns,Rose Bush", "Tetraphobia,Rosebud,Tetraphobia"])); 
+}
+if (levelupselections.indexOf("Boosterpack:Dazzling Light,Twinkle,Twinkle") > -1){ 
+  Rules.substitute("Backfire", "Radar"); 
+  Rules.substitute("Bop,Bop,Bop", rand(["Twinkle,Solar Blessing,Twinkle", "Twinkle,Flashlight,Twinkle", "Solar Blessing,Twinkle,Solar Blessing"])); 
+}
+if (levelupselections.indexOf("boosterpack:Rusty Broadsword,Stumble,Stumble") > -1){ 
+  Rules.substitute("Backfire", "Rusty Shield"); 
+  Rules.substitute("Bop,Bop,Bop", rand(["Stumble,Blood Let,Stumble", "Stumble,Sour Candy,Stumble", "Blood Let,Blood Let,Blood Let"])); 
+}
+if (levelupselections.indexOf("boosterpack:Starburst,Starshine,Starshine") > -1){ 
+  Rules.substitute("Backfire", "Starsear"); 
+  Rules.substitute("Bop,Bop,Bop", rand(["Starshine,Starspear,Starshine", "Ignite,One With Everything,Ignite", "Blood Let,Infestation,Blood Let"])); 
+}
+if (levelupselections.indexOf("boosterpack:Mind Blast,Precognition,Precognition") > -1){ 
+  Rules.substitute("Backfire", "Recognition"); 
+  Rules.substitute("Bop,Bop,Bop", rand(["Sour Candy,Precognition,Sour Candy", "Knitting Needle,Precognition,Knitting Needle", "Boxing Gloves,Precognition,Boxing Gloves"])); 
+}
+if (levelupselections.indexOf("boosterpack:Transmogrify,Dud,Dud") > -1){ 
+  Rules.substitute("Backfire", "Polymorph"); 
+  Rules.substitute("Bop,Bop,Bop", rand(["Dud,Arbitrage,Dud", "Worm Bite,Arbitrage,Worm Bite", "Dud,Stealth,Dud"])); 
+}
+if (levelupselections.indexOf("Boosterpack:Eviscerate,Nosebleed,Nosebleed") > -1){ 
+  Rules.substitute("Backfire", "Paper Cut"); 
+  Rules.substitute("Bop,Bop,Bop", rand(["Nosebleed,Blood Breath,Nosebleed", "Nosebleed,Slice and Dice,Nosebleed", "Slice and Dice,Blunt Sword,Slice and Dice"])); 
+}
