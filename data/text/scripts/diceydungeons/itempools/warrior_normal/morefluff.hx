0ce5549cafc4bebdd3c4112329613c40e4f76797
JuniorUdale <junior_udale@hotmail.com>
2021-11-23 13:30:34 +0000

    various stuff

--- data/text/scripts/diceydungeons/itempools/warrior_normal/morefluff.hx
+++ data/text/scripts/diceydungeons/itempools/warrior_normal/morefluff.hx
@@ -1,54 +1,23 @@
-var database = loaddata("morefluff/generators/maingenerator.csv");
-var warriorshops = [];
-var strangeshop = [];
-var awesomelist = [];
-var floor2gooditem = [];
-var floor3item = [];
-var floor5item = [];
-var vampireitem = [];
+var database = loaddata("morefluff/generators/main_generator.csv");
+
+var pooltargets = ["warriorshops","strangeshop","awesomelist","floor2gooditem","floor3item","floor5item","vampireitem"];
+var pools = [[],[],[],[],[],[],[]];
 
 for(item in database){
 	if(item.warrior == "YES" && item.vanilla == "YES"){
-		var tags = item.vanillatags.split(",");
-		if(tags.indexOf("warriorshops") > -1){
-			warriorshops.push(item.name);
-		}
-		if(tags.indexOf("strangeshop") > -1){
-			strangeshop.push(item.name);
-		};
-		if(tags.indexOf("awesomelist") > -1){
-			awesomelist.push(item.name);
-		};
-		if(tags.indexOf("floor2gooditem") > -1){
-			floor2gooditem.push(item.name);
-		};
-		if(tags.indexOf("floor3item") > -1){
-			floor3item.push(item.name);
-		};
-		if(tags.indexOf("floor5item") > -1){
-			floor5item.push(item.name);
+		var tags = item.tags.split(",");
+		var generators = item.generators.split(",");
+		var i = 0;
+		while(i < generators.length){
+			if(generators[i] == "warrior_normal"){
+				var target = pooltargets.indexOf(tags[i]);
+				if(target != -1){
+					pools[target].push(item.name);
+				}
+			}
+			i+=1;
 		}
 	}
 }
 
-var myitempools = [warriorshops, strangeshop, awesomelist, floor2gooditem, floor3item, floor5item, vampireitem];
-return myitempools
-
-
-/*var warriorshops = ["Pike", "Zweihander", "Halberd", "Chakram", "Paintbrush", "Mace", "Pavise", "Natural Charm", "Loaded Dice", "Shotgun","Boomstick", "Blooplicate", "Trowel", "Mallet", "Frosted Dirk", "Spiked Sword", "Electroplating", "Trishula", "Tomahawk","Cat O' Six Tails", "Divine Blade", "Healing Shiv", "Venomous Goop", "Really Spiky Shield", "Nether Portal", "Ornamental Shield", "Execute", "Spikemail", "Hairdryer", "Shielding Potion", "Field Dressing", "Staff", "Plague Stave", "Asklepios","Shield Toss","Draining Crystal","Saxophone","Evocation","Revoke","Explosive Shield","Karambit","Molten Staff","Lockdown","Bloodclaw","Fruit Shooter","Juggernaut","Taunting Sword","Vine Whip","Differing Thorns","Mirror Coating","Shrug","Metal Slam","Solar Panel","Wooden Mallet","Holy Fire","Spearslinger","Sludge","Icicle Fall","Reinforce","Chitin Helmet","Sneksword","Freegen","Spire Shield"];
-
-var strangeshop = ["Power Through", "drowS", "Expose Weakness", "Budge", "Metallicize", "Resonator",  "Leech Seed", "Panic Button", "Jade Blade", "Malediction", "Evil Eye", "Incapacitate","Chronoblade","Diamond Wall","Convergence","Shieldsplosion","Exoskeleton","First Sit","Hyena Siren"];
-
-var awesomelist = ["Talons", "Labrys", "Targe Charge", "Canesword", "Urumi", "Crass Knuckles","Metronome"];
-
-var floor2gooditem = ["Promote"];
-
-var floor3item = ["Bolster", "Finisher"];
-
-var floor5item = ["Shammer", "Doppliduplicateinate","Shield Scrap"];
-
-var vampireitem = [];
-
-var myitempools = [warriorshops, strangeshop, awesomelist, floor2gooditem, floor3item, floor5item, vampireitem];
-
-return myitempools;*/
+return pools;
---
+++ data/text/scripts/morefluff/items/archmage.hx
@@ -0,0 +1,12 @@
+/* Script for the item "Archmage's Rune". */
+
+var total = args[0];
+
+
+/* make a random item with countdown <= total
+activate that item
+then re-call archmage with the remaining
+if it gets too low, call a fluff item
+
+
+*/
