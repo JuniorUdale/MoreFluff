6ab87cc86ac423c3520e597cdfe55efd3c3dd3a2
JuniorUdale <junior_udale@hotmail.com>
2021-11-23 16:46:28 +0000

    blade of destiny first look official 2021 free no virus 360p mp4

--- data/text/scripts/morefluff/items/archmage.hx
+++ data/text/scripts/morefluff/items/archmage.hx
@@ -2,11 +2,13 @@
 
 var total = args[0];
 
+var l = getequipmentlist(null,["small"],["witchonly","robotonly","inventoronly","thiefonly","warrioronly","jesteronly","excludefromrandomlists","alternateversion"]);
+
 
 /* make a random item with countdown <= total
 activate that item
 then re-call archmage with the remaining
-if it gets too low, call a fluff item
+if it gets too low, call a fluff item (Junk Spell)
 
 
 */
